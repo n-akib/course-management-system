ed770db20482f8b4e6d55e4785222a0afce5b8cd
n-akib <n.mahfuze@gmail.com>
2025-05-12 13:31:09 +0600

    Project Setup and DB connection.

---
+++ src/main/java/com/akib/courseManagementSystem/CourseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.akib.courseManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class CourseManagementSystemApplication {
+
+	public static void main(String[] args) {
+				SpringApplication.run(CourseManagementSystemApplication.class, args);
+	}
+}
